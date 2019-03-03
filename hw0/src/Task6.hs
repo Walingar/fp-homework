3bbc991e4de67564626786456376c8f88ef8f5e7
Nikolay.Rykunov <Nikolay.Rykunov@jetbrains.com>
2019-03-03 23:22:28 +0300

    Solve Task6

--- hw0/src/Task6.hs
+++ hw0/src/Task6.hs
@@ -1,14 +1,20 @@
-module Task6 where
+module Task6
+  ( x1
+  , x2
+  , foo
+  ) where
 
 import Data.Maybe (mapMaybe)
 import Task1 (distributivity)
 
+-- (Left ("harold" ++ " hide " ++ "the " ++ "pain")), Left ("harold" ++ " hide " ++ "the " ++ "pain")))
 x1 = distributivity (Left ("harold" ++ " hide " ++ "the " ++ "pain"))
 
+-- False
 x2 = null $ mapMaybe foo "pole chudes ochen' chudesno"
 
 foo :: Char -> Maybe Double
 foo char =
   case char == 'o' of
-    True  -> Just $ exp pi
+    True -> Just $ exp pi
     False -> Nothing
