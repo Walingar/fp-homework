2cf9c5e86fa58394c18acd362bc64bbcff8ba7c7
Nikolay.Rykunov <Nikolay.Rykunov@jetbrains.com>
2019-03-03 23:32:39 +0300

    Solve Task5

---
+++ hw1/src/Task5.hs
@@ -0,0 +1,103 @@
+{-# LANGUAGE InstanceSigs #-}
+
+module Task5
+  ( maybeConcat
+  , eitherConcat
+  , fromString
+  , toString
+  ) where
+
+-- TODO tests
+maybeConcat :: [Maybe [a]] -> [a]
+maybeConcat = foldr f []
+  where
+    f :: Maybe [a] -> [a] -> [a]
+    f Nothing list    = list
+    f (Just cur) list = cur ++ list
+
+eitherConcat :: (Monoid a, Monoid b) => [Either a b] -> (a, b)
+eitherConcat = foldr f (mempty, mempty)
+  where
+    f :: (Monoid a, Monoid b) => Either a b -> (a, b) -> (a, b)
+    f (Left left) (a, b)   = (a <> left, b)
+    f (Right right) (a, b) = (a, b <> right)
+
+data NonEmpty a =
+  a :| [a]
+
+data ThisOrThat a b
+  = This a
+  | That b
+  | Both a
+         b
+
+instance Semigroup (NonEmpty a) where
+  (<>) :: NonEmpty a -> NonEmpty a -> NonEmpty a
+  (<>) (x :| xs) (y :| ys) = x :| (xs ++ (y : ys))
+
+instance (Semigroup a, Semigroup b) => Semigroup (ThisOrThat a b) where
+  (<>) :: ThisOrThat a b -> ThisOrThat a b -> ThisOrThat a b
+  (<>) (This a) (This b)         = This (a <> b)
+  (<>) (This a) (That b)         = Both a b
+  (<>) (That a) (This b)         = Both b a
+  (<>) (That a) (That b)         = That (a <> b)
+  (<>) (This a) (Both x y)       = Both (a <> x) y
+  (<>) (That b) (Both x y)       = Both x (b <> y)
+  (<>) (Both x y) (This a)       = Both (x <> a) y
+  (<>) (Both x y) (That b)       = Both x (y <> b)
+  (<>) (Both x1 y1) (Both x2 y2) = Both (x1 <> x2) (y1 <> y2)
+
+newtype Name =
+  Name String
+
+instance Semigroup Name where
+  (<>) :: Name -> Name -> Name
+  (<>) (Name "") (Name b) = Name b
+  (<>) (Name a) (Name "") = Name a
+  (<>) (Name a) (Name b)  = Name (a ++ ('.' : b))
+
+instance Monoid Name where
+  mempty :: Name
+  mempty = Name ""
+
+newtype Endo a = Endo
+  { getEndo :: a -> a
+  }
+
+instance Semigroup (Endo a) where
+  (<>) :: Endo a -> Endo a -> Endo a
+  (<>) (Endo a) (Endo b) = Endo $ a . b
+
+instance Monoid (Endo a) where
+  mempty :: Endo a
+  mempty = Endo id
+
+data Builder
+  = One Char
+  | Many [Builder]
+
+instance Semigroup Builder where
+  (<>) :: Builder -> Builder -> Builder
+  (<>) ch1@(One _) ch2@(One _)   = Many [ch1, ch2]
+  (<>) ch@(One _) (Many [])      = ch
+  (<>) ch@(One _) (Many list)    = Many $ ch : list
+  (<>) (Many []) ch@(One _)      = ch
+  (<>) (Many list) ch@(One _)    = Many $ list ++ [ch]
+  (<>) (Many list1) (Many list2) = Many $ list1 ++ list2
+
+instance Monoid Builder where
+  mempty :: Builder
+  mempty = Many []
+
+fromString :: String -> Builder
+fromString = foldr f mempty
+  where
+    f :: Char -> Builder -> Builder
+    f current builder = One current <> builder
+
+toString :: Builder -> String
+toString (One ch) = [ch]
+toString (Many list) = foldr f "" list
+  where
+    f :: Builder -> String -> String
+    f builder st = toString builder ++ st
