05348c0a1de3ff670e3abe57f30e0e808eefd4f6
Nikolay.Rykunov <Nikolay.Rykunov@jetbrains.com>
2019-03-04 19:13:40 +0300

    Fix warnings

--- hw0/src/Task6.hs
+++ hw0/src/Task6.hs
@@ -8,9 +8,11 @@ import Data.Maybe (mapMaybe)
 import Task1 (distributivity)
 
 -- (Left ("harold" ++ " hide " ++ "the " ++ "pain")), Left ("harold" ++ " hide " ++ "the " ++ "pain")))
+x1 :: (Either String b, Either String c)
 x1 = distributivity (Left ("harold" ++ " hide " ++ "the " ++ "pain"))
 
 -- False
+x2 :: Bool
 x2 = null $ mapMaybe foo "pole chudes ochen' chudesno"
 
 foo :: Char -> Maybe Double
