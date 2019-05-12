85b45400cb6e4b382506ce7d017eda10af7fe9c4
Nikolay.Rykunov <Nikolay.Rykunov@jetbrains.com>
2019-05-12 19:30:43 +0300

    Solve task1

--- hw4/src/Task1.hs
+++ hw4/src/Task1.hs
@@ -1,11 +1,17 @@
+{-# LANGUAGE BangPatterns #-}
+
 module Task1
   ( multiply
   , multiplySimple
   ) where
 
-import Data.List (transpose)
 import Control.Monad.ST
-import Data.Array.ST
+import Data.Foldable (forM_)
+import Data.List (transpose)
+import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as M
+import qualified Data.Vector.Unboxed as VU
+import qualified Data.Vector.Unboxed.Mutable as MU
 
 multiply :: [[Int]] -> [[Int]] -> Maybe [[Int]]
 multiply a b =
@@ -13,28 +19,53 @@ multiply a b =
     then Nothing
     else Just $ multiply' a b
 
-type IntArray s = STUArray s Int Int
+type IntArray s = MU.MVector s Int
 
-type Matrix s  = STUArray s Int (ST s (IntArray s))
+type Matrix s = ST s (M.MVector s (IntArray s))
 
 toIntArray :: [Int] -> ST s (IntArray s)
-toIntArray list = do
-  let listSize = length list
-  newListArray (0, listSize - 1) list
+toIntArray list = VU.thaw $ VU.fromList list
 
-toMatrix :: [[Int]] -> ST s (STUArray s Int (ST s (IntArray s)))
-toMatrix list = do
-  let listSize = length list
-  k <- newListArray (0, listSize - 1) (map toIntArray list)
-  undefined
+toMatrix :: [[Int]] -> Matrix s
+toMatrix matrix = do
+  lists <- traverse toIntArray matrix
+  V.thaw $ V.fromList lists
+
+emptyMatrix' :: Int -> Int -> Matrix s
+emptyMatrix' n k =
+  let line = replicate k 0
+   in toMatrix (replicate n line)
+
+intArrayToList :: IntArray s -> ST s [Int]
+intArrayToList list = do
+  freeze <- VU.freeze list
+  return $ VU.toList freeze
+
+fromMatrix :: M.MVector s (IntArray s) -> ST s [[Int]]
+fromMatrix list = do
+  freeze <- V.freeze list
+  list' <- traverse intArrayToList freeze
+  return $ V.toList list'
 
 multiply' :: [[Int]] -> [[Int]] -> [[Int]]
-multiply' a b = runST $ do
-  let n = length a
-  let k = length $ head b
-  a' <- toMatrix a
-  b' <- toMatrix b
-  undefined
+multiply' a b =
+  runST $ do
+    let l = length a
+    let m = length $ head a
+    let n = length $ head b
+    a' <- toMatrix a
+    b' <- toMatrix b
+    c <- emptyMatrix' l n
+    forM_ [0 .. l - 1] $ \i -> do
+      aRow <- M.read a' i
+      forM_ [0 .. n - 1] $ \j ->
+        forM_ [0 .. m - 1] $ \r -> do
+          bRow <- M.read b' r
+          !aElement <- MU.read aRow r
+          !bElement <- MU.read bRow j
+          cRow <- M.read c i
+          MU.modify cRow (+ (aElement * bElement)) j
+    fromMatrix c
 
 checkMatrix :: [[Int]] -> [[Int]] -> Bool
 checkMatrix a b
--- hw4/test/Task1Spec.hs
+++ hw4/test/Task1Spec.hs
@@ -1,6 +1,6 @@
 module Task1Spec where
 
-import Task1 (multiplySimple)
+import Task1 (multiplySimple, multiply)
 import Test.Hspec
 
 spec :: Spec
@@ -11,3 +11,5 @@ spec = do
      ([[1, 7, 56, 2], [19, -2, 12, -1]] `multiplySimple` [[-123, -12, 2]]) `shouldBe` Nothing
   it "simplemultiply tests" $
      ([[1, 7, 56, 2], [19, -2, 12, -1]] `multiplySimple` [[-123, -12, 2], [23, 4, 1], [-2, -13, 2], [3, 1, 4]]) `shouldBe` Just [[-68, -710, 129], [-2410, -393, 56]]
+  it "multiply tests" $
+    ([[1, 7, 56, 2], [19, -2, 12, -1]] `multiply` [[-123, -12, 2], [23, 4, 1], [-2, -13, 2], [3, 1, 4]]) `shouldBe` Just [[-68, -710, 129], [-2410, -393, 56]]
